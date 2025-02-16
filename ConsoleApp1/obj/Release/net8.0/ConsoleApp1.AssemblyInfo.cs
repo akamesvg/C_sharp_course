29f07fbb86489a6ae469348c0b293104b46b4424
akamesvg <akamesvg@gmail.com>
2025-02-17 00:53:11 +0800

    2

--- ConsoleApp1/obj/Release/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Release/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a3d305f04d0e9c8a528440bd007f1c28e902736")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
