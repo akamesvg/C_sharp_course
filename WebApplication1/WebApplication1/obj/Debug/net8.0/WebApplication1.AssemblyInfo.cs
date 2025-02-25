22e957638c3c0d5b172174f7ec4352eef3a6d1f4
akamesvg <akamesvg@gmail.com>
2025-02-26 03:47:45 +0800

    ЛР6

--- WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d0e0f0d99430aa49ddf40bb91e76a26af2565a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6716fbcbce389c8278e6e4c750c9219ae1c50041")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
